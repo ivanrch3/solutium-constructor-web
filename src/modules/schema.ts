227ddcf8fa2bde88c3eb11f76358d1a36a799b98
ivanrch3 <ivanrch2@gmail.com>
2026-04-02 21:42:28 -0600

    reseteo

--- src/modules/schema.ts
+++
@@ -1,6 +0,0 @@
-import { ModuleDefinition, getModuleDefinition } from './registry';
-
-export const getModuleSchema = (type: string) => {
-  const def = getModuleDefinition(type);
-  return def?.schema || [];
-};
